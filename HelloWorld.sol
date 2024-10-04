1a775d6817aab1c288ea8ee5056410a3a584c29c
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-05 01:08:14 +0300

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,7 +1,7 @@
-//SPDX-License-Identifier:MIT;
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.24;
 
   contract HelloWorld{
-    function() public view retuns{
-        return = "Hello World";}
+    function print () public pure retuns(string memory){
+        return "Hello World";}
         }
