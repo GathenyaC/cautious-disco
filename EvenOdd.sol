bf65be35c200c4f6433e688edbfe2f3b6694cf22
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-05 15:17:57 +0300

    Create EvenOdd.sol

---
+++ EvenOdd.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Idendtifier : GPL-3.O
+  pragma solidity ^0.8.24;
+
+      contract Evenodd{
+          function check(int num) public pure returns {
+            if(num % 2 ==0)
+              returns "EVEN";
+            return "ODD";
+            }
+          }
