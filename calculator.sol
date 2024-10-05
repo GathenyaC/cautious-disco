52443a42253798a9d8a2d920c356dca3dd8c0e74
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-05 15:10:43 +0300

    Create calculator.sol

---
+++ calculator.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: GPL-3.0
+
+  pragma solidity ^0.8.24;
+
+    contract   Calculator {
+     function add(int a, int b) public pure returns (int) {
+         return (a+b);
+         }
+
+      function sub(int a,int b) public pure retuens{
+         return(a-b);
+       }
+
+
+      function mul(int a, int b) public pure returns {
+         return(a*b);
+         }
+
+     function div(int a, int b) public pure returns {
+        return (a/b);
+        }
+
+      }
+
+      
