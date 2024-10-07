3af04b2064548cde67dc024e3ba0b4be97869f4b
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-07 12:10:37 -0500

    Create Evennumb.sol

---
+++ Evennumb.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity
+
+    contract EvenNumber {
+              function findEven(numb) public pure returns (bool){
+                  if (num < 2) return false;
+                      for(i = 2; i <= num; ) {
+                        if (num % 2) return true;
+                        }
+                      }
