f225a8189dbe00141a96348446d92b8ae3cfa3dc
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-09 10:58:10 +0300

    Create voting.sol

---
+++ voting.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity ^0.8.24;
+
+  contract voting{
+
+    struct voter{
+        uint weight;
+        bool voted;
+        address delegate;
+        uint vote;
+        }
+
+    struct Proposal{
+          bytes32 name;
+          uint VoteCount;
+          }
+
+    address public chairperson;
+
+//declares a variable that stores "voter" struct or each possible address
+
+    mapping(address => voter) public voters;
+
+//A dynamically-sized array of 'Proposal' structs
+
+proposal [] public proposals;
+
+//create a new ballot to choose one of the 'ProposalNames'
+      constructo
+      
