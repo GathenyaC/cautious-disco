bd1c3b754ce7b468d6dcd39e6faac561b84e96b4
Gathenya <101094546+GathenyaC@users.noreply.github.com>
2024-10-09 11:07:46 +0300

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -29,4 +29,16 @@ proposal [] public proposals;
       constructor (bytes32[] memory proposalNames) {
           chairperson = msg.sender;
           voters[chairperson].weight = 1;
+
+//for each of the proposalNames create a new proposal object and add it to the end of the array
+
+    for (uint i = 0; i< proposalNames.length; i++) {
+
+      proposal.push(Proposal({
+        name : proposalNames [i],
+        voteCount :0
+        }));
+ }
+}
+
       
